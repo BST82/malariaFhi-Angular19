178d17843ef285bb752e87a763f2fb1666eadb8f
Bhagyashri Tamrakar <bt7355711982@gmail.com>
2025-03-28 11:22:05 +0530

    created componnets for dashboard,products,Stock Allocation
    Stock Transfer
    
    Stock Request Raised to Requirement  formc

---
+++ src/app/component/form/stock-request-raised/stock-request-raised.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-request-raised',
+  imports: [],
+  templateUrl: './stock-request-raised.component.html',
+  styleUrl: './stock-request-raised.component.scss'
+})
+export class StockRequestRaisedComponent {
+
+}
---
+++ src/app/component/form/stock-transfer-form/stock-transfer-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-transfer-form',
+  imports: [],
+  templateUrl: './stock-transfer-form.component.html',
+  styleUrl: './stock-transfer-form.component.scss'
+})
+export class StockTransferFormComponent {
+
+}
